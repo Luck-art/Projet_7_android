f481d90088b0d777335c6293bcee1f50e03211bf
Lucas <potronlucas9@outlook.fr>
2023-02-17 16:34:13 +0100

    Bug fix for Facebook

--- app/go4food/app/src/main/java/com/luck_art/go4food/ui/repository/UserRepository.java
+++ app/go4food/app/src/main/java/com/luck_art/go4food/ui/repository/UserRepository.java
@@ -1,2 +1,99 @@
-package com.luck_art.go4food.ui.repository;public class UserRepository {
+package com.luck_art.go4food.ui.repository;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+
+public final class UserRepository {
+
+	private static final String COLLECTION_NAME = "users";
+	private static final String USERNAME_FIELD = "username";
+	private static volatile UserRepository instance;
+
+	// Get the Collection Reference
+	private CollectionReference getUsersCollection(){
+		return FirebaseFirestore.getInstance().collection(COLLECTION_NAME);
+	}
+
+	// Create User in Firestore
+	public void createUser() {
+		FirebaseUser user = getCurrentUser();
+		if(user != null){
+			String urlPicture = (user.getPhotoUrl() != null) ? user.getPhotoUrl().toString() : null;
+			String username = user.getDisplayName();
+			String uid = user.getUid();
+
+			//User userToCreate = new User(uid, username, urlPicture);
+
+			Task<DocumentSnapshot> userData = getUserData();
+
+		}
+	}
+
+	// Get User Data from Firestore
+	public Task<DocumentSnapshot> getUserData(){
+		String uid = this.getCurrentUser().getUid();
+		if(uid != null){
+			return this.getUsersCollection().document(uid).get();
+		}else{
+			return null;
+		}
+	}
+
+	// Update User Username
+	public Task<Void> updateUsername(String username) {
+		String uid = this.getCurrentUser().getUid();
+		if(uid != null){
+			return this.getUsersCollection().document(uid).update(USERNAME_FIELD, username);
+		}else{
+			return null;
+		}
+	}
+
+
+	// Delete the User from Firestore
+	public void deleteUserFromFirestore() {
+		String uid = this.getCurrentUser().getUid();
+		if(uid != null){
+			this.getUsersCollection().document(uid).delete();
+		}
+	}
+
+	private UserRepository() { }
+
+	public static UserRepository getInstance() {
+		UserRepository result = instance;
+		if (result != null) {
+			return result;
+		}
+		synchronized(UserRepository.class) {
+			if (instance == null) {
+				instance = new UserRepository();
+			}
+			return instance;
+		}
+	}
+
+	@Nullable
+	public FirebaseUser getCurrentUser(){
+		return FirebaseAuth.getInstance().getCurrentUser();
+	}
+
+	public Task<Void> signOut(Context context){
+		return AuthUI.getInstance().signOut(context);
+	}
+
+	public Task<Void> deleteUser(Context context){
+		return AuthUI.getInstance().delete(context);
+	}
+
 }
